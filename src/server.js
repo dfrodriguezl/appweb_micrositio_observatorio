1fcfefe94ba4245524cd2216ec5f72158ae59bff
Ivan Carrillo - Personal PC <ivancho4321@gmail.com>
2021-09-06 14:00:39 -0500

    versión inicial

---
+++ src/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+
+//backend en el puerto 3000
+app.listen(3000, function () {
+  console.log(Date().toString());
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+        resolve: {
+          extensions:['.js','.jsx']
+        }
+
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/index.html',
+      append: false,
+      chunks: ["index"]
+    }),
+    
+
+    /*
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', //para que lo haga sólo al momento de hacer el build
+      openAnalyzer: true, //para que nos muestre el resultado inmediatamente
+    }),
+    */
+
+    //new CompressionPlugin()
+    
+  ],
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript 
+  // minifying and other thing so let's set mode to development
+  mode: 'production'
+};
