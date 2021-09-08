6c73f050ef653a9d1e846657d45f682688ecdbd7
Alejo1117525453# <alejozuleta9217@gmail.com>
2021-09-08 11:55:21 -0500

    cambios

--- src/views/common/buttonredwine.js
+++ src/views/common/buttonredwine.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const ButtonRedWine = () => {
-    const MyButton = styled.h4`
-    padding: 2rem;
-    text-aling: center;
+    const MyButtonRedWine = styled.button`
+    padding: 1vh;
+    border-radius: 2vh;
+    background-color: #821a3f;
+    color: white;
+    font-weight: bold;
+    font-family: 'Work sans';
+    font-size: 2vh;
+    text-transform: capitalize;
+    transition:all 0.8s ease-out;
+    cursor: pointer;
+    &:hover{
+        background-color: ${"#db709380"};
+    }
     `;
     return (
-    <MyButton>Acerca de</MyButton>)
+    <MyButtonRedWine>Acerca de</MyButtonRedWine>)
 }
 
 export default ButtonRedWine
--- src/views/pages/contentgrid.js
+++ src/views/pages/contentgrid.js
@@ -1,5 +1,4 @@
 import { Grid } from '@material-ui/core';
-import Paper from "@material-ui/core/Paper";
 import React, { Component } from 'react';
 import city from '../../img/city.svg';
 import styles from '../../css/Top.module.css';
@@ -9,21 +8,42 @@ const Title = "¿Qué es el Observatorio Inmobiliario Nacional?"
 const Text = "Es una infraestructura de datos capaz de consolidar aplicando ténicas registrales, la información que proviene de los observatorios inmobiliarios locales y otros actores clave, conforme a estándares establecidos, disponerlos de manera oficiao, agregando valor mediante la consolidación, gestión de sus propios datos y aplicación de nuevos métodos analíticos"
 
 const Top_content = () => <Grid>
-    <Paper className={styles.paper}>
         <div className={styles.imagen_top}>
             <h3>{Title}</h3>
             <p>{Text}</p>
             <ButtonRedWine/>
         </div>
-    </Paper>
 </Grid>
 
-const Center_content = () => <Grid>
-<Paper className={styles.paper}>Center</Paper>
+const Center_content = () => <Grid container direction="row" spacing={2}>    
+    <Grid item container direction="column" xs spacing={2}>
+        <Grid item xs> 
+            <div className={styles.containerTall}><h1>Seguimiento</h1></div>
+        </Grid>
+        <Grid item xs> 
+            <div className={styles.containerTall}><h1>Comunicación</h1></div>            
+        </Grid>        
+    </Grid>
+    <Grid item container direction="column" xs spacing={2}>
+        <Grid item xs > 
+            <div className={styles.containerTitle}><h1>Ventajas</h1></div>
+        </Grid>
+        <Grid item xs> 
+            <div className={styles.containerImg}></div>            
+        </Grid>        
+    </Grid>
+    <Grid item container direction="column" xs spacing={2}>
+        <Grid item xs> 
+            <div className={styles.containerTall}><h1>Integración</h1></div>
+        </Grid>
+        <Grid item xs> 
+            <div className={styles.containerTall}><h1>LADM</h1></div>            
+        </Grid>        
+    </Grid>
 </Grid>
 
 const Bottom_content = () => <Grid>
-<Paper className={styles.paper}>Inferior</Paper>
+Inferiori
 </Grid>
 
 const ContentGrid = () =>
