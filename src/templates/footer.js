03f2aed2cf6e3f2efdefd4670e6906e1a061b7a7
Luis Carlos <luiscardozoicg@gmail.com>
2021-09-13 16:27:49 -0500

    Nueva Actualizacion Navbar y footer

--- src/templates/footer.js
+++ src/templates/footer.js
@@ -1,52 +1,75 @@
 import React from 'react'
 import Styled from "styled-components";
-import Grid from '@material-ui/core/Grid';
+import {Grid, Box} from '@material-ui/core/';
+import { HiOutlineLocationMarker } from "react-icons/hi";
 import { AiOutlinePhone, AiOutlineClockCircle } from "react-icons/ai";
 import logoGobernment from "../img/logoGovernment.png";
 
 
 const ContenedorFooter= Styled.div`
-        background-color: #fff;
-        padding: 380px 0 0 5px; 
+        background-color: #FAFAFA;
+        margin-top:15px;
+        
+                
+`;
+const H3 = Styled.h3`
+    text-align: center;
+    padding: 15px 0;
 `;
 const Imagen = Styled.img`
     max-width:200px;
+    
 `;
 
 const Footer = () =>{
     return(
         <ContenedorFooter>
-            <h3>Departamento Administrativo Nacional de Estadistica DANE</h3>
-            <Grid container spacing={3}>
+            <H3>Departamento Administrativo Nacional de Estadistica DANE</H3>
+            <Grid container>
                 {/* <span>Departamento Administrativo Nacional de Estadistica DANE</span> */}
-                <Grid item xs={6} sm={3}>
-                    
-                    <p><AiOutlinePhone/>
-                        <span> call center</span>
-                        Conmutador: (571) 597 833 00 ó (571)5978398
-                        linea gratuita nacional 01 8000 912 002
-                    </p>
+                <Grid item xs={6} sm={3}  >                    
+                    <Grid  container >
+                        <Grid item sm={4} ><Box pt={1} pl={8} color="#B91450" fontSize = {35}><AiOutlinePhone/> </Box>
+                        </Grid>
+                        <Grid item sm={8}   >
+                           <Box p={1} color="black" fontSize = {17} fontWeight={700}> CALL CENTER</Box>
+                               <Box color="black" fontSize = {17} fontWeight={700}> Conmutador:</Box> (571) 597 833 00 ó (571)5978398
+                               <Box color="black" fontSize = {17} fontWeight={700}> linea gratuita nacional </Box>01 8000 912 002
+                            
+                        </Grid>
+                    </Grid>
                 </Grid>
                 <Grid item xs={6} sm={3}>
-                    
-                    <p><AiOutlineClockCircle/>
-                        <span> HORARIO DE ATENCIÓN</span>
-                        Lunes a viernes 8:00 - 17:00
-                    </p>
+                    <Grid  container >
+                            <Grid item sm={4} ><Box pt={1} pl={8} color="#B91450" fontSize = {35}><AiOutlineClockCircle/> </Box>
+                            </Grid>
+                            <Grid item sm={8}   >
+                            <Box p={1} color="black" fontSize = {17} fontWeight={700}> HORARIO DE ATENCIÓN</Box>
+                                <Box color="black" fontSize = {17} fontWeight={700}> Lunes a viernes</Box>
+                                8:00 - 17:00
+                                
+                            </Grid>
+                    </Grid>      
                 </Grid>
                 <Grid item xs={6} sm={3}>
-                    
-                    <p>
-                        <span> DIRECCIÓN</span>
-                        Carrera 59 No. 26-70 Interior I - CAN
-                        Código postal: 111321
-                        Apartado Aéreo: 80043
-                        Bogotá D.C., Colombia - Suramérica
-                    </p>
+                     <Grid  container >
+                            <Grid item sm={4} ><Box pt={1} pl={8} color="#B91450" fontSize = {35}><HiOutlineLocationMarker/> </Box>
+                            </Grid>
+                            <Grid item sm={8}   >
+                            <Box p={1} color="black" fontSize = {17} fontWeight={700}> DIRECCIÓN</Box>
+                                <Box color="black" fontSize = {17} fontWeight={700}>Carrera 59 No. 26-70 Interior I - CAN</Box>
+                                <Box color="black" fontSize = {17} fontWeight={700}>Código postal:</Box>
+                                111321
+                                <Box color="black" fontSize = {17} fontWeight={700}>Apartado Aéreo:</Box>
+                                80043
+                                <Box color="black" fontSize = {17} fontWeight={700}>Bogotá D.C., Colombia - Suramérica</Box>
+                            </Grid>
+                     </Grid>                    
                 </Grid>
                 <Grid item xs={6} sm={3}>
-                    <Imagen src= {logoGobernment} />
-                  
+                    <Box >
+                        <Imagen src= {logoGobernment} />
+                    </Box>
                 </Grid>
             </Grid>
         </ContenedorFooter>
