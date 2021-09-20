df61169eaa71f8e597b597d8c0e4e2f8a347f5e2
Luis Carlos <luiscardozoicg@gmail.com>
2021-09-20 12:41:46 -0500

    Separacion de iconos navbar-modificacion input buscar header

--- src/templates/Navbar.js
+++ src/templates/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Grid, Box} from '@material-ui/core/';
 import { AiFillHome, AiOutlineDown } from "react-icons/ai";
 import {
   BrowserRouter as Router,
@@ -7,7 +8,20 @@ import {
   Link,
   NavLink 
 } from "react-router-dom";
+import { makeStyles, 
+  ThemeProvider,
+   createTheme } from '@material-ui/core/styles';
 import Styled from "styled-components";
+
+const useStyle = makeStyles({
+  root:{
+    textDecoration: 'none',
+    margin: '5px',
+    padding: '5px', 
+    color: 'inherit'
+  }
+
+});
 
 const NavBarContenedor = Styled.ul`    
     max-width:1300px;
@@ -22,15 +36,9 @@ const NavBarContenedor = Styled.ul`
 
 const MenuA = Styled.a`
     text-decoration: none;
-    margin: 15px;
-    padding: 10px; 
-    color: #000; 
-    &:hover{
-      background-color: #BD0B4E;
-      border-radius: 3px;
-      color: #fff;
-      /* font-weight: bold; */
-    }        
+    margin: 5px;
+    padding: 5px; 
+    color: inherit;            
 `;
 
 const MenuIL =  Styled.li`
@@ -44,13 +52,23 @@ background-color: #f9f9f9;
 min-width: 160px;
 box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
 z-index: 1;
-margin: 10px 0 0 15px;
+top: 40px;
+left:2px;
 `;  
 const DropDownLi = Styled(MenuIL)`
-    display: inline-block;   
+    /* display: inline-block;    */
+    position:relative;
+    display: flex;
+    /* justify-content: center;
+    align-items:center; */
     &:hover ${DropDownContent} {
       display: block;
-    }
+    },
+    /* &:first-child {
+        color:red;
+        
+        
+    } */
   `;
 const SubMenuA = Styled.a`
    color: black;
@@ -62,8 +80,23 @@ const SubMenuA = Styled.a`
     background-color: #f1f1f1;
   }
 `;
+const UtilidadCentrarTexto = Styled.div`
+    position:relative;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    &:hover{
+      background-color: #BD0B4E;
+      border-radius: 3px;
+      color: #fff;
+      /* font-weight: bold; */
+    } 
+`;
+
+  
 
 const Navbar = () => {
+  const classes = useStyle();
   
   const handleClick = action => {
     if (!action) return;
@@ -74,59 +107,84 @@ const Navbar = () => {
   return (
     
         <NavBarContenedor>
-            <DropDownLi><MenuA href ="#"><AiFillHome/>Inicio </MenuA>
-                  
-                <DropDownContent>                        
-                          {" "}
-                          <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
-                        <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
-                       <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
-                </DropDownContent>
+            <DropDownLi>
+              <UtilidadCentrarTexto>
+                  <AiFillHome/>
+                  <MenuA href ="#">Inicio </MenuA>
+                      
+                    <DropDownContent>                        
+                              {" "}
+                              <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
+                            <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
+                          <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
+                    </DropDownContent>
+               </UtilidadCentrarTexto>
             </DropDownLi>
-            <DropDownLi><MenuA href ="#"><AiOutlineDown/>Servicios </MenuA>
+
+            <DropDownLi>
+            <UtilidadCentrarTexto>
+                  <AiOutlineDown/>
+                  <MenuA href ="#">Servicios </MenuA>
                   
                 <DropDownContent>                        
-                          {" "}
-                          <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
-                        <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
-                       <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
-                </DropDownContent>
+                              {" "}
+                              <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
+                            <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
+                          <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
+                    </DropDownContent>
+                </UtilidadCentrarTexto>
             </DropDownLi>
-            <DropDownLi><NavLink to="/Mapas"><MenuA href ="#"><AiOutlineDown/>Mapas </MenuA></NavLink>
-                  
-                <DropDownContent>                        
-                          {" "}
-                          <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
-                        <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
-                       <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
-                </DropDownContent>
+            <DropDownLi>
+                <UtilidadCentrarTexto>
+                    <AiOutlineDown/>
+                    <NavLink to="/Mapas" className = {classes.root} >Mapas</NavLink>
+                        
+                      <DropDownContent>                        
+                                {" "}
+                                <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
+                              <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
+                            <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
+                      </DropDownContent>
+                 </UtilidadCentrarTexto>
             </DropDownLi>
-            <DropDownLi><MenuA href ="#"><AiOutlineDown/>Catastro Multiproposito </MenuA>
-                  
-                <DropDownContent>                        
-                          {" "}
-                          <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
-                        <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
-                       <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
-                </DropDownContent>
+            <DropDownLi>
+                <UtilidadCentrarTexto>
+                  <AiOutlineDown/>
+                  <MenuA href ="#">Catastro Multiproposito </MenuA>
+                      
+                    <DropDownContent>                        
+                              {" "}
+                              <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
+                            <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
+                          <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
+                    </DropDownContent>
+                </UtilidadCentrarTexto>
             </DropDownLi>
-            <DropDownLi><MenuA href ="#"><AiOutlineDown/>Boletines </MenuA>
-                  
-                <DropDownContent>                        
-                          {" "}
-                          <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
-                        <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
-                       <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
-                </DropDownContent>
+            <DropDownLi>
+              <UtilidadCentrarTexto>
+                  <AiOutlineDown/>
+                  <MenuA href ="#">Boletines </MenuA>
+                      
+                    <DropDownContent>                        
+                              {" "}
+                              <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
+                            <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
+                          <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
+                    </DropDownContent>
+               </UtilidadCentrarTexto>
             </DropDownLi>
-            <DropDownLi><MenuA href ="#"><AiOutlineDown/>Ingresar </MenuA>
-                  
-                <DropDownContent>                        
-                          {" "}
-                          <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
-                        <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
-                       <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
-                </DropDownContent>
+            <DropDownLi>
+              <UtilidadCentrarTexto>
+                  <AiOutlineDown/>
+                  <MenuA href ="#">Ingresar </MenuA>
+                      
+                    <DropDownContent>                        
+                              {" "}
+                              <SubMenuA href ="#" onClick={() => this.handleClick("Link1")}>Link 1 </SubMenuA>
+                            <SubMenuA href ="#"  onClick={() => this.handleClick("Link2")} >Link 2 </SubMenuA>
+                          <SubMenuA href ="#"  onClick={() => this.handleClick("Link3")} >Link 3  </SubMenuA>
+                    </DropDownContent>
+                </UtilidadCentrarTexto>
             </DropDownLi>               
         </NavBarContenedor>
         
