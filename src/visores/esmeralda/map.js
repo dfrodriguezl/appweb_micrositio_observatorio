b1dcc8b44f7fa8b89da9280f3a25eb2e36e376ba
Alejo1117525453# <alejozuleta9217@gmail.com>
2021-11-05 09:29:15 -0500

    map

--- src/visores/esmeralda/map.js
+++ src/visores/esmeralda/map.js
@@ -8,8 +8,8 @@ mapboxgl.accessToken =
 const Map = () => {
   const mapContainerRef = useRef(null);
 
-  const [lng, setLng] = useState(-74.083);
-  const [lat, setLat] = useState(4.70);
+  const [lng, setLng] = useState(-74.5447);
+  const [lat, setLat] = useState(40.6892);
   const [zoom, setZoom] = useState(6);
 
   //Constructor del mapa
@@ -24,6 +24,44 @@ const Map = () => {
     // Navegacion ( +/- zoom )
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
     
+    map.on('load', () => {
+      map.addSource('wms-test-source', {
+      'type': 'raster',
+      // use the tiles option to specify a WMS tile source URL
+      // https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/
+      'tiles': [
+      'https://img.nj.gov/imagerywms/Natural2015?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=Natural2015'
+      ],
+      'tileSize': 256
+      });
+      map.addLayer(
+      {
+      'id': 'wms-test-layer',
+      'type': 'raster',
+      'source': 'wms-test-source',
+      'paint': {},      
+      }, 
+      // 'aeroway-line'
+      );
+      });
+
+      map.on('load', () => {
+        map.addSource('my-data', {
+          "type": "geojson",
+          "data": {
+          "type": "Feature",
+          "geometry": {
+          "type": "Point",
+          "coordinates": [-77.0323, 38.9131]
+          },
+          "properties": {
+          "title": "Mapbox DC",
+          "marker-symbol": "monument"
+          }
+          }
+          });
+      });
+
     map.on('click', ({point}) => {
       const features = map.queryRenderedFeatures(point, {
         layers:['puntos']
@@ -40,7 +78,8 @@ const Map = () => {
       )
       .addTo(map);
 
-    });
+    });   
+
     // Clean up on unmount
     return () => map.remove();
   }, []); 
