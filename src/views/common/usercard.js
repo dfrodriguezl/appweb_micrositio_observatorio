c4ff25e40588484f47fa4b4629e07ccb5aabeb41
Nataly <nasarmientoo@dane.gov.co>
2024-05-24 16:44:53 -0500

    🚧 Validadores funcionando hasta hoja NPH

--- src/views/common/usercard.js
+++ src/views/common/usercard.js
@@ -88,8 +88,8 @@ import databigsource from "./databig.js";
 import ExportExcel from "react-export-excel-fixed-xlsx";
 
 const ExcelFile = ExportExcel.ExcelFile;
-const ExcelSheet = ExportExcel.ExcelSheet;
-const ExcelColumn = ExportExcel.ExcelColumn;
+const ExcelSheet = ExportExcel.ExcelFile.ExcelSheet;
+const ExcelColumn = ExportExcel.ExcelFile.ExcelColumn;
 const markerUrl =
   "https://js.devexpress.com/Demos/RealtorApp/images/map-marker.png";
 
